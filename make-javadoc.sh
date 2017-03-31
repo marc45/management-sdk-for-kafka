07f6f5df749740e0eb1b7b126b5e0ad88f6731ee
Hugo Villalba <hugo.villalba@intel.com>
2017-03-31 12:15:44 -0300

    Add trademark footer to Javadoc

--- make-javadoc.sh
+++ make-javadoc.sh
@@ -4,4 +4,4 @@ mkdir  -p javadoc
 
 project_version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | egrep -v '^\[|Downloading:' | sed 's/[^0-9\.]//g' | awk 1 ORS='')
 
-javadoc -sourcepath ./src/main/java/ -subpackages com/mcafee/dxl/streaming/operations/client -d ./javadoc -classpath ./target/msdk4kafka-${project_version}-jar-with-dependencies.jar
+javadoc -bottom '<p><i>Kafka and Zookeeper and other names and brands may be claimed as the property of others</i></p>' -sourcepath ./src/main/java/ -subpackages com/mcafee/dxl/streaming/operations/client -d ./javadoc -classpath ./target/msdk4kafka-${project_version}-jar-with-dependencies.jar
